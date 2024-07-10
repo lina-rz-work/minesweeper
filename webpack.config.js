9bb8f398ae5ebf548ceddcc9651dbb5a539d8445
Alina Mukhamatkhanova <lina.rz.work@gmail.com>
2024-07-10 19:36:38 +0500

    updated webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default {
-    mode: 'development',
-    entry: './src/index.ts',
-    output: {
-        filename: 'bundle.js',
-    },
-    module: {
-		rules: [
-			{
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js']
-	},
-	watch: true,
-}
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', 
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '', globOptions: { ignore: ['**/index.html'] } },
+      ],
+    }),
+  ],
+};
